d0a86ce088f5d9031a55bd933c1a9ca914573a2b
sofia amoroso <sofi79a0@stud.kea.dk>
2023-05-23 15:56:34 +0200

    cv link in the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link';
 
 export default function Navbar(){
     return (
         <nav className={styles.nav} >
             <h3>Hello!</h3>
             <ul className={styles.listnav}>
+                <li>
+            <Link href="sofia_amoroso_cv.pdf" target="_blank">
+              <p>Check my CV</p>
+            </Link>
+          </li>
                 <li> <a href="#projects">Projects</a></li>
             </ul>
         </nav>
