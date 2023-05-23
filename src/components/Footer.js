682846f9c14e9c843aa896a1ec8a68cb1bf45c68
sofia amoroso <sofi79a0@stud.kea.dk>
2023-05-23 15:53:13 +0200

    link to cv

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,11 @@ const Footer = () => {
         <footer id="footer" className={styles.footer}>
         <h3>Get in Contact!</h3>
         <ul className={styles.listnav}>
+        <li>
+            <Link href="sofia_amoroso_cv.pdf" target="_blank">
+              <p>Check my CV</p>
+            </Link>
+          </li>
           <li>
             <Link href="https://www.linkedin.com/in/sofia-amoroso/" target="_blank">
               <p>Linkedin</p>
